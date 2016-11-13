22b8407bdc9455bb30692d61f2209c723c0664f4
peroxy <peroxylol@gmail.com>
2016-11-14 00:40:39 +0100

    Added WCF BonService.

---
+++ BonService/BonService/BonService.svc.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BonService
+{
+    public class BonService : IBonService
+    {
+        private enum Row
+        {
+            Name = 0,
+            Address = 1,
+            Phone = 2,
+            Price = 3,
+            CoordinateX = 4,
+            CoordinateY = 5,
+            UpdatedOn = 6,
+            WeekStart = 7,
+            WeekEnd = 8,
+            SaturdayStart = 9,
+            SaturdayEnd = 10,
+            SundayStart = 11,
+            SundayEnd = 12,
+            Soup = 13,
+            MainCourse = 14,
+            Salad = 15,
+            Dessert = 16,
+            Lunch = 17,
+            SaladBar = 18,
+            Vegetarian = 19,
+            Disabled = 20,
+            DisabledWc = 21,
+            Pizzas = 22,
+            Weekends = 23,
+            FastFood = 24,
+            StudentBenefits = 25,
+            Delivery = 26
+        }
+
+        public Restaurants GetAllRestaurants()
+        {
+            return GetFilteredRestaurants(new Filter());
+        }
+
+        public Restaurants GetCurrentlyOpenRestaurants(DateTime now)
+        {
+            var ts = new TimeSpan(now.Hour, now.Minute, now.Second);
+            switch (now.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                    return GetFilteredRestaurants(new Filter
+                    {
+                        OpeningTime = new OpeningTime
+                        {
+                            Saturday = new Saturday
+                            {
+                                From = ts,
+                                To = ts
+                            }
+                        }
+                    });
+                case DayOfWeek.Sunday:
+                    return GetFilteredRestaurants(new Filter
+                    {
+                        OpeningTime = new OpeningTime
+                        {
+                            Sunday = new Sunday
+                            {
+                                From = ts,
+                                To = ts
+                            }
+                        }
+                    });
+                default:
+                    return GetFilteredRestaurants(new Filter
+                    {
+                        OpeningTime = new OpeningTime
+                        {
+                            Week = new Week
+                            {
+                                From = ts,
+                                To = ts
+                            }
+                        }
+                    });
+            }
+            
+        }
+
+        public Restaurants GetFilteredRestaurants(Filter values)
+        {
+            string sql = GetSqlCommand(values);
+            Debug.WriteLine(sql);
+            using (
+                var conn =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["BonDBConnectionString"].ConnectionString))
+            {
+                conn.Open();
+                var cmd = new SqlCommand(sql, conn) {CommandType = CommandType.Text};
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    var restaurants = new Restaurants();
+                    while (reader.Read())
+                    {
+                        restaurants.Values.Add(new Restaurant
+                        {
+                            Address = reader.GetString((int) Row.Address),
+                            CoordinateX = reader.GetDecimal((int) Row.CoordinateX),
+                            CoordinateY = reader.GetDecimal((int) Row.CoordinateY),
+                            HasDelivery = reader.GetBoolean((int) Row.Delivery),
+                            HasDisabledSupport = reader.GetBoolean((int) Row.Disabled),
+                            HasDisabledWcSupport = reader.GetBoolean((int) Row.DisabledWc),
+                            HasSaladBar = reader.GetBoolean((int) Row.SaladBar),
+                            HasStudentBenefits = reader.GetBoolean((int) Row.StudentBenefits),
+                            HasVegetarianSupport = reader.GetBoolean((int) Row.Vegetarian),
+                            Menu = new Menu
+                            {
+                                Dessert =
+                                    reader.IsDBNull((int) Row.Dessert) ? null : reader.GetString((int) Row.Dessert),
+                                MainCourse =
+                                    reader.IsDBNull((int) Row.MainCourse)
+                                        ? null
+                                        : reader.GetString((int) Row.MainCourse),
+                                Salad = reader.IsDBNull((int) Row.Salad) ? null : reader.GetString((int) Row.Salad),
+                                Soup = reader.IsDBNull((int) Row.Soup) ? null : reader.GetString((int) Row.Soup),
+                            },
+                            Name = reader.GetString((int) Row.Name),
+                            OpenDuringWeekends = reader.GetBoolean((int) Row.Weekends),
+                            OpeningTime = new OpeningTime
+                            {
+                                Week = new Week
+                                {
+                                    From =
+                                        reader.IsDBNull((int) Row.WeekStart)
+                                            ? (TimeSpan?) null
+                                            : reader.GetTimeSpan((int) Row.WeekStart),
+                                    To =
+                                        reader.IsDBNull((int) Row.WeekEnd)
+                                            ? (TimeSpan?) null
+                                            : reader.GetTimeSpan((int) Row.WeekEnd)
+                                },
+                                Saturday = new Saturday
+                                {
+                                    From =
+                                        reader.IsDBNull((int) Row.SaturdayStart)
+                                            ? (TimeSpan?) null
+                                            : reader.GetTimeSpan((int) Row.SaturdayStart),
+                                    To =
+                                        reader.IsDBNull((int) Row.SaturdayEnd)
+                                            ? (TimeSpan?) null
+                                            : reader.GetTimeSpan((int) Row.SaturdayEnd)
+                                },
+                                Sunday = new Sunday
+                                {
+                                    From =
+                                        reader.IsDBNull((int) Row.SundayStart)
+                                            ? (TimeSpan?) null
+                                            : reader.GetTimeSpan((int) Row.SundayStart),
+                                    To =
+                                        reader.IsDBNull((int) Row.SundayEnd)
+                                            ? (TimeSpan?) null
+                                            : reader.GetTimeSpan((int) Row.SundayEnd)
+                                }
+                            },
+                            Phone = reader.IsDBNull((int) Row.Phone) ? null : reader.GetString((int) Row.Phone),
+                            Price = reader.GetDecimal((int) Row.Price),
+                            ServesFastFood = reader.GetBoolean((int) Row.FastFood),
+                            ServesLunch = reader.GetBoolean((int) Row.Lunch),
+                            ServesPizzas = reader.GetBoolean((int) Row.Pizzas),
+                            UpdatedOn = reader.GetDateTime((int) Row.UpdatedOn)
+                        });
+                    }
+                    return restaurants;
+                }
+            }
+        }
+
+        private static string GetSqlCommand(Filter filter)
+        {
+            var sql = new StringBuilder("SELECT * FROM V_RESTAURANT WHERE ");
+
+            if (filter.Address != null)
+            {
+                sql.Append($"R_Address LIKE '%{filter.Address}%' AND ");
+            }
+
+            if (filter.Name != null)
+            {
+                sql.Append($"R_Name LIKE '%{filter.Name}%' AND ");
+            }
+
+            if (filter.Price != null)
+            {
+                sql.Append($"R_Price BETWEEN '{filter.Price.From}' AND '{filter.Price.To}' AND ");
+            }
+
+            if (filter.Coordinates != null)
+            {
+                if (filter.Coordinates.X != null)
+                {
+                    sql.Append(
+                        $"R_CoordinateX BETWEEN '{filter.Coordinates.X.From}' AND '{filter.Coordinates.X.To}' AND ");
+                }
+                if (filter.Coordinates.Y != null)
+                {
+                    sql.Append(
+                        $"R_CoordinateY BETWEEN '{filter.Coordinates.Y.From}' AND '{filter.Coordinates.Y.To}' AND ");
+                }
+            }
+
+            if (filter.OpeningTime != null)
+            {
+                if (filter.OpeningTime.Week != null)
+                {
+                    sql.Append(
+                        $"O_WeekStart <= '{filter.OpeningTime.Week.From}' AND O_WeekEnd >= '{filter.OpeningTime.Week.To}' AND ");
+                }
+                if (filter.OpeningTime.Saturday != null)
+                {
+                    sql.Append(
+                        $"O_SaturdayStart <= '{filter.OpeningTime.Saturday.From}' AND O_SaturdayEnd >= '{filter.OpeningTime.Saturday.To}' AND ");
+                }
+                if (filter.OpeningTime.Sunday != null)
+                {
+                    sql.Append(
+                        $"O_SundayStart <= '{filter.OpeningTime.Sunday.From}' AND O_SundayEnd >= '{filter.OpeningTime.Sunday.To}' AND ");
+                }
+            }
+
+            if (filter.HasDelivery)
+            {
+                sql.Append("F_Delivery = 1 AND ");
+            }
+            if (filter.HasDisabledSupport)
+            {
+                sql.Append("F_Disabled = 1 AND ");
+            }
+            if (filter.HasDisabledWcSupport)
+            {
+                sql.Append("F_DisabledWC = 1 AND ");
+            }
+            if (filter.ServesFastFood)
+            {
+                sql.Append("F_FastFood = 1 AND ");
+            }
+            if (filter.ServesLunch)
+            {
+                sql.Append("F_Lunch = 1 AND ");
+            }
+            if (filter.ServesPizzas)
+            {
+                sql.Append("F_Pizzas = 1 AND ");
+            }
+            if (filter.HasSaladBar)
+            {
+                sql.Append("F_SaladBar = 1 AND ");
+            }
+            if (filter.HasStudentBenefits)
+            {
+                sql.Append("F_StudentBenefits = 1 AND ");
+            }
+            if (filter.HasVegetarianSupport)
+            {
+                sql.Append("F_Vegetarian = 1 AND ");
+            }
+            if (filter.OpenDuringWeekends)
+            {
+                sql.Append("F_Weekends = 1 AND ");
+            }
+
+            sql.Remove(sql.Length - 5, 5);
+            return sql.ToString();
+        }
+    }
+}
---
+++ BonService/BonService/IBonService.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BonService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IBonService
+    {
+        [OperationContract]
+        Restaurants GetAllRestaurants();
+
+        [OperationContract]
+        Restaurants GetFilteredRestaurants(Filter values);
+
+        [OperationContract]
+        Restaurants GetCurrentlyOpenRestaurants(DateTime now);
+    }
+
+    [DataContract]
+    public class Restaurants
+    {
+        [DataMember]
+        public List<Restaurant> Values { get; set; } = new List<Restaurant>();
+    }
+
+    [DataContract]
+    public class Restaurant
+    {
+        [DataMember]
+        public string Name { get; set; } 
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public string Phone { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+
+        [DataMember]
+        public decimal CoordinateX { get; set; }
+
+        [DataMember]
+        public decimal CoordinateY { get; set; }
+
+        [DataMember]
+        public OpeningTime OpeningTime { get; set; }
+
+        [DataMember]
+        public DateTime UpdatedOn { get; set; }
+
+        [DataMember]
+        public Menu Menu { get; set; }
+
+        [DataMember]
+        public bool ServesLunch { get; set; }
+
+        [DataMember]
+        public bool HasSaladBar { get; set; }
+
+        [DataMember]
+        public bool HasVegetarianSupport { get; set; }
+
+        [DataMember]
+        public bool HasDisabledSupport { get; set; }
+
+        [DataMember]
+        public bool HasDisabledWcSupport { get; set; }
+
+        [DataMember]
+        public bool ServesPizzas { get; set; }
+
+        [DataMember]
+        public bool OpenDuringWeekends { get; set; }
+
+        [DataMember]
+        public bool ServesFastFood { get; set; }
+
+        [DataMember]
+        public bool HasStudentBenefits { get; set; }
+
+        [DataMember]
+        public bool HasDelivery { get; set; }
+    }
+
+    [DataContract]
+    public class Filter
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public Price Price { get; set; }
+
+        [DataMember]
+        public Coordinates Coordinates{ get; set; }
+
+        [DataMember]
+        public OpeningTime OpeningTime { get; set; }
+
+        [DataMember]
+        public bool ServesLunch { get; set; }
+
+        [DataMember]
+        public bool HasSaladBar { get; set; }
+
+        [DataMember]
+        public bool HasVegetarianSupport { get; set; }
+
+        [DataMember]
+        public bool HasDisabledSupport { get; set; }
+
+        [DataMember]
+        public bool HasDisabledWcSupport { get; set; }
+
+        [DataMember]
+        public bool ServesPizzas { get; set; }
+
+        [DataMember]
+        public bool OpenDuringWeekends { get; set; }
+
+        [DataMember]
+        public bool ServesFastFood { get; set; }
+
+        [DataMember]
+        public bool HasStudentBenefits { get; set; }
+
+        [DataMember]
+        public bool HasDelivery { get; set; }
+    }
+
+    [DataContract]
+    public class Coordinates
+    {
+        [DataMember]
+        public Coordinate X { get; set; }
+
+        [DataMember]
+        public Coordinate Y { get; set; }
+    }
+
+    [DataContract]
+    public class Coordinate
+    {
+        [DataMember]
+        public decimal From { get; set; }
+
+        [DataMember]
+        public decimal To { get; set; }
+    }
+
+    [DataContract]
+    public class Price
+    {
+        [DataMember]
+        public decimal From { get; set; }
+
+        [DataMember]
+        public decimal To { get; set; }
+    }
+
+    [DataContract]
+    public class OpeningTime
+    {
+        [DataMember]
+        public Week Week { get; set; }
+
+        [DataMember]
+        public Saturday Saturday { get; set; }
+
+        [DataMember]
+        public Sunday Sunday { get; set; }
+    }
+
+    [DataContract]
+    public class Week
+    {
+        [DataMember]
+        public TimeSpan? From { get; set; }
+
+        [DataMember]
+        public TimeSpan? To { get; set; }
+    }
+
+    [DataContract]
+    public class Saturday
+    {
+        [DataMember]
+        public TimeSpan? From { get; set; }
+
+        [DataMember]
+        public TimeSpan? To { get; set; }
+    }
+
+    [DataContract]
+    public class Sunday
+    {
+        [DataMember]
+        public TimeSpan? From { get; set; }
+
+        [DataMember]
+        public TimeSpan? To { get; set; }
+    }
+
+    [DataContract]
+    public class Menu
+    {
+        [DataMember]
+        public string Soup { get; set; }
+
+        [DataMember]
+        public string MainCourse { get; set; }
+
+        [DataMember]
+        public string Salad { get; set; }
+
+        [DataMember]
+        public string Dessert { get; set; }
+
+    }
+}
