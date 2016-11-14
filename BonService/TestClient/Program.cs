defcbd7801ce3115a375e5b0b0cc20afabf3bc07
peroxy <peroxylol@gmail.com>
2016-11-14 01:12:20 +0100

    Added an example test client for the webservice.

---
+++ BonService/TestClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TestClient.BonWs;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new BonServiceClient(new BasicHttpBinding(),
+                new EndpointAddress("http://bonwebservice.azurewebsites.net/BonService.svc?wsdl"));
+            Restaurants response = client.GetFilteredRestaurants(new Filter
+            {
+                HasDelivery = true
+            });
+
+            foreach (Restaurant restaurant in response.Values)
+            {
+                Console.WriteLine(restaurant.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
