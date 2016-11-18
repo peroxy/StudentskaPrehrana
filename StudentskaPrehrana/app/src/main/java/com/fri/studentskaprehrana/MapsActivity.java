7e77f2993dca859f99fce504fe279fb40c455810
clutchy <ziga.jerina@gmail.com>
2016-11-18 02:39:45 +0100

    Maps Work

--- StudentskaPrehrana/app/src/main/java/com/fri/studentskaprehrana/MapsActivity.java
+++ StudentskaPrehrana/app/src/main/java/com/fri/studentskaprehrana/MapsActivity.java
@@ -2,6 +2,7 @@ package com.fri.studentskaprehrana;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
